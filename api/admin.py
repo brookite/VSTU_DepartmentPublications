1ceaf4620aa9cf0afde3caacf9764814e35d4fe6
brookit <brookit18@gmail.com>
2024-04-27 13:59:24 +0300

    feat(admin): Filled admin panel

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import *
+
+admin.site.register(Author)
+admin.site.register(Publication)
+admin.site.register(Department)
+admin.site.register(Faculty)
+admin.site.register(Settings)
+admin.site.register(Tag)
+admin.site.register(EmailSubscriber)
+admin.site.register(AuthorAlias)
--- api/models.py
+++ api/models.py
@@ -6,11 +6,19 @@ class Faculty(models.Model):
     name = models.CharField(max_length=96)
     library_id = models.IntegerField()
 
+    class Meta:
+        verbose_name = "Факультет"
+        verbose_name_plural = "Факультеты"
+
 
 class Department(models.Model):
     name = models.CharField(max_length=96)
     faculty = models.ForeignKey(Faculty, on_delete=models.CASCADE)
     library_id = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Кафедра"
+        verbose_name_plural = "Кафедры"
 
 
 class Author(models.Model):
@@ -19,15 +27,27 @@ class Author(models.Model):
     library_primary_name = models.CharField(max_length=96)
     department = models.ForeignKey(Department, null=True, on_delete=models.SET_NULL)
 
+    class Meta:
+        verbose_name = "Автор"
+        verbose_name_plural = "Авторы"
+
 
 class AuthorAlias(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     alias = models.CharField(max_length=96)
 
+    class Meta:
+        verbose_name = "Псевдоним автора"
+        verbose_name_plural = "Псевдонимы автора"
+
 
 class Tag(models.Model):
     name = models.CharField(max_length=32)
     authors = ManyToManyField(Author)
+
+    class Meta:
+        verbose_name = "Тег"
+        verbose_name_plural = "Теги"
 
 
 class Publication(models.Model):
@@ -36,10 +56,18 @@ class Publication(models.Model):
     added_date = models.DateTimeField(auto_now_add=True)
     department = models.ForeignKey(Department, null=True, on_delete=models.SET_NULL)
 
+    class Meta:
+        verbose_name = "Публикация"
+        verbose_name_plural = "Публикации"
+
 
 class EmailSubscriber(models.Model):
     email = models.CharField(max_length=96)
     tags = models.ManyToManyField(Tag)
+
+    class Meta:
+        verbose_name = "Подписчик на обновления"
+        verbose_name_plural = "Подписчики на обновления"
 
 
 class Timestamps(models.Model):
@@ -54,3 +82,7 @@ class ShortUpdateTasks(models.Model):
 class Settings(models.Model):
     param_name = models.CharField(max_length=64, primary_key=True, unique=True)
     param_value = models.CharField(max_length=1024, null=True)
+
+    class Meta:
+        verbose_name = "Параметр системы"
+        verbose_name_plural = "Параметры системы"
